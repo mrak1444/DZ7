2c353e23eb51b06cdae2f23e745bf341ca7d934e
Mrak1444 <Mrak1444@mail.ru>
2020-11-24 15:44:57 +0600

    Очередные изменения в ДЗ

--- DZ7/Form1.Designer.cs
+++ DZ7/Form1.Designer.cs
@@ -29,15 +29,22 @@ namespace DZ7
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnCommand1 = new System.Windows.Forms.Button();
             this.btnCommand2 = new System.Windows.Forms.Button();
             this.btnReset = new System.Windows.Forms.Button();
             this.lblNumber = new System.Windows.Forms.Label();
+            this.StepNum = new System.Windows.Forms.Label();
+            this.Game = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.TimerGame = new System.Windows.Forms.Label();
+            this.StopGame = new System.Windows.Forms.Button();
+            this.RndNum = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnCommand1
             // 
-            this.btnCommand1.Location = new System.Drawing.Point(196, 12);
+            this.btnCommand1.Location = new System.Drawing.Point(83, 12);
             this.btnCommand1.Name = "btnCommand1";
             this.btnCommand1.Size = new System.Drawing.Size(75, 23);
             this.btnCommand1.TabIndex = 0;
@@ -47,7 +54,7 @@ namespace DZ7
             // 
             // btnCommand2
             // 
-            this.btnCommand2.Location = new System.Drawing.Point(196, 99);
+            this.btnCommand2.Location = new System.Drawing.Point(83, 41);
             this.btnCommand2.Name = "btnCommand2";
             this.btnCommand2.Size = new System.Drawing.Size(75, 23);
             this.btnCommand2.TabIndex = 1;
@@ -57,7 +64,7 @@ namespace DZ7
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(196, 189);
+            this.btnReset.Location = new System.Drawing.Point(83, 70);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(75, 23);
             this.btnReset.TabIndex = 2;
@@ -68,17 +75,73 @@ namespace DZ7
             // lblNumber
             // 
             this.lblNumber.AutoSize = true;
-            this.lblNumber.Location = new System.Drawing.Point(58, 105);
+            this.lblNumber.Location = new System.Drawing.Point(22, 47);
             this.lblNumber.Name = "lblNumber";
             this.lblNumber.Size = new System.Drawing.Size(46, 17);
             this.lblNumber.TabIndex = 3;
             this.lblNumber.Text = "label1";
+            // 
+            // StepNum
+            // 
+            this.StepNum.AutoSize = true;
+            this.StepNum.Location = new System.Drawing.Point(12, 195);
+            this.StepNum.Name = "StepNum";
+            this.StepNum.Size = new System.Drawing.Size(46, 17);
+            this.StepNum.TabIndex = 4;
+            this.StepNum.Text = "label1";
+            // 
+            // Game
+            // 
+            this.Game.Location = new System.Drawing.Point(176, 12);
+            this.Game.Name = "Game";
+            this.Game.Size = new System.Drawing.Size(95, 52);
+            this.Game.TabIndex = 5;
+            this.Game.Text = "button1";
+            this.Game.UseVisualStyleBackColor = true;
+            this.Game.Click += new System.EventHandler(this.Game_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // TimerGame
+            // 
+            this.TimerGame.AutoSize = true;
+            this.TimerGame.Location = new System.Drawing.Point(225, 195);
+            this.TimerGame.Name = "TimerGame";
+            this.TimerGame.Size = new System.Drawing.Size(46, 17);
+            this.TimerGame.TabIndex = 6;
+            this.TimerGame.Text = "label1";
+            // 
+            // StopGame
+            // 
+            this.StopGame.Location = new System.Drawing.Point(176, 70);
+            this.StopGame.Name = "StopGame";
+            this.StopGame.Size = new System.Drawing.Size(95, 23);
+            this.StopGame.TabIndex = 7;
+            this.StopGame.Text = "button1";
+            this.StopGame.UseVisualStyleBackColor = true;
+            this.StopGame.Click += new System.EventHandler(this.StopGame_Click);
+            // 
+            // RndNum
+            // 
+            this.RndNum.AutoSize = true;
+            this.RndNum.Location = new System.Drawing.Point(112, 144);
+            this.RndNum.Name = "RndNum";
+            this.RndNum.Size = new System.Drawing.Size(46, 17);
+            this.RndNum.TabIndex = 8;
+            this.RndNum.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(283, 224);
+            this.Controls.Add(this.RndNum);
+            this.Controls.Add(this.StopGame);
+            this.Controls.Add(this.TimerGame);
+            this.Controls.Add(this.Game);
+            this.Controls.Add(this.StepNum);
             this.Controls.Add(this.lblNumber);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnCommand2);
@@ -96,6 +159,12 @@ namespace DZ7
         private System.Windows.Forms.Button btnCommand2;
         private System.Windows.Forms.Button btnReset;
         private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label StepNum;
+        private System.Windows.Forms.Button Game;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label TimerGame;
+        private System.Windows.Forms.Button StopGame;
+        private System.Windows.Forms.Label RndNum;
     }
 }
 
--- DZ7/Form1.cs
+++ DZ7/Form1.cs
@@ -12,29 +12,70 @@ namespace DZ7
 {
     public partial class Form1 : Form
     {
+        int _count;
         public Form1()
         {
             InitializeComponent();
             btnCommand1.Text = "+1";
             btnCommand2.Text = "x2";
+            Game.Text = "Играть";
             btnReset.Text = "Сброс";
             lblNumber.Text = "0";
+            StepNum.Text = "0";
+            TimerGame.Text = "0";
+            RndNum.Text = "Загаданное число";
             this.Text = "Удвоитель";
         }
 
         private void btnCommand1_Click(object sender, EventArgs e)
         {
             lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
+            StepNum.Text = (int.Parse(StepNum.Text) + 1).ToString();
+            Game.Text = "Стоп";
         }
 
         private void btnCommand2_Click(object sender, EventArgs e)
         {
             lblNumber.Text = (int.Parse(lblNumber.Text) * 2).ToString();
+            StepNum.Text = (int.Parse(StepNum.Text) + 1).ToString();
+            Game.Text = "Стоп";
         }
 
         private void btnReset_Click(object sender, EventArgs e)
         {
             lblNumber.Text = "1";
+            StepNum.Text = (int.Parse(StepNum.Text) + 1).ToString();
+            Game.Text = "Стоп";
+        }
+
+        private void Game_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            if (timer1.Enabled)
+            {
+                timer1.Tick += Tick;
+                Game.Text = "Сдаться";
+                Random rnd = new Random();
+                int value = rnd.Next(1, 50);
+                RndNum.Text = value.ToString();
+            }
+            else
+            {
+                timer1.Tick -= Tick;
+                Game.Text = "Играть";
+                TimerGame.Text = "0";
+                _count = 0;
+            }
+        }
+
+        private void Tick(object t, EventArgs eventArgs)
+        {
+            TimerGame.Text = (++_count).ToString();
+        }
+
+        private void StopGame_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
