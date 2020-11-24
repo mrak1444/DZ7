5fa3ed20b843b635943ffe65ebe2a2b9f5d7267d
Mrak1444 <Mrak1444@mail.ru>
2020-11-24 14:26:37 +0600

    доработка программы1

--- DZ7/Form1.Designer.cs
+++ DZ7/Form1.Designer.cs
@@ -37,35 +37,38 @@ namespace DZ7
             // 
             // btnCommand1
             // 
-            this.btnCommand1.Location = new System.Drawing.Point(64, 12);
+            this.btnCommand1.Location = new System.Drawing.Point(196, 12);
             this.btnCommand1.Name = "btnCommand1";
             this.btnCommand1.Size = new System.Drawing.Size(75, 23);
             this.btnCommand1.TabIndex = 0;
             this.btnCommand1.Text = "button1";
             this.btnCommand1.UseVisualStyleBackColor = true;
+            this.btnCommand1.Click += new System.EventHandler(this.btnCommand1_Click);
             // 
             // btnCommand2
             // 
-            this.btnCommand2.Location = new System.Drawing.Point(64, 41);
+            this.btnCommand2.Location = new System.Drawing.Point(196, 99);
             this.btnCommand2.Name = "btnCommand2";
             this.btnCommand2.Size = new System.Drawing.Size(75, 23);
             this.btnCommand2.TabIndex = 1;
             this.btnCommand2.Text = "button2";
             this.btnCommand2.UseVisualStyleBackColor = true;
+            this.btnCommand2.Click += new System.EventHandler(this.btnCommand2_Click);
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(64, 70);
+            this.btnReset.Location = new System.Drawing.Point(196, 189);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(75, 23);
             this.btnReset.TabIndex = 2;
             this.btnReset.Text = "button3";
             this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
             // lblNumber
             // 
             this.lblNumber.AutoSize = true;
-            this.lblNumber.Location = new System.Drawing.Point(12, 47);
+            this.lblNumber.Location = new System.Drawing.Point(58, 105);
             this.lblNumber.Name = "lblNumber";
             this.lblNumber.Size = new System.Drawing.Size(46, 17);
             this.lblNumber.TabIndex = 3;
@@ -75,7 +78,7 @@ namespace DZ7
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(153, 106);
+            this.ClientSize = new System.Drawing.Size(283, 224);
             this.Controls.Add(this.lblNumber);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnCommand2);
--- DZ7/Form1.cs
+++ DZ7/Form1.cs
@@ -15,6 +15,26 @@ namespace DZ7
         public Form1()
         {
             InitializeComponent();
+            btnCommand1.Text = "+1";
+            btnCommand2.Text = "x2";
+            btnReset.Text = "Сброс";
+            lblNumber.Text = "0";
+            this.Text = "Удвоитель";
+        }
+
+        private void btnCommand1_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
+        }
+
+        private void btnCommand2_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = (int.Parse(lblNumber.Text) * 2).ToString();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = "1";
         }
     }
 }
